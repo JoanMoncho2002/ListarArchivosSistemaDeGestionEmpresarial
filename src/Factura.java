46525694e3a5d2b43572758dc5bae7aab608331d
Joan Moncho <joanmonvi@gmail.com>
2023-02-28 18:31:59 +0100

    aaads

--- src/Factura.java
+++ src/Factura.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Factura {
+import java.util.Date;
+
+public class Factura {
+    private String numeroFactura;
+    private String numeroCliente;
+    private Date fecha;
+
+    public Factura(String numeroFactura, String numeroCliente, Date fecha) {
+        this.numeroFactura = numeroFactura;
+        this.numeroCliente = numeroCliente;
+        this.fecha = fecha;
+    }
+
+    public String getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(String numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public String getNumeroCliente() {
+        return numeroCliente;
+    }
+
+    public void setNumeroCliente(String numeroCliente) {
+        this.numeroCliente = numeroCliente;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Ruta de la carpeta a listar
+        String rutaCarpeta = "C:\\mi_carpeta";
+
+        // Crear objeto File para la carpeta
+        File carpeta = new File(rutaCarpeta);
+
+        // Obtener lista de archivos en la carpeta
+        File[] listaArchivos = carpeta.listFiles();
+
+        // Imprimir nombre de cada archivo en la lista
+        for (File archivo : listaArchivos) {
+            if (archivo.isFile()) {
+                System.out.println(archivo.getName());
+            }
+        }
     }
+
+    /*public class GeneradorNumeroFactura {
+        private int ultimoNumeroFactura = 0;
+
+        public String generarNumeroFactura(String numeroCliente, LocalDate fecha) {
+            String ffff = String.format("%04d", ++ultimoNumeroFactura);
+            String cccc = numeroCliente;
+            String ddmmaa = fecha.format(DateTimeFormatter.ofPattern("ddMMyy"));
+            return ffff + "-" + cccc + "-" + ddmmaa;
+        }
+    }*/
 }
